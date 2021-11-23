02e305af0f3b0780667b524092b9952dd9622d51
Alexander Bauer <94492708+alexanderbauer89@users.noreply.github.com>
2021-11-23 16:28:40 +0200

    Update 1480_Running_Sum_of_1d_Array.py

--- Problems/1480_Running_Sum_of_1d_Array.py
+++ Problems/1480_Running_Sum_of_1d_Array.py
@@ -1,5 +1,6 @@
 # Runtime: 36 ms, faster than 88.82% of Python3 online submissions for Running Sum of 1d Array.
 # Memory Usage: 14.2 MB, less than 89.96% of Python3 online submissions for Running Sum of 1d Array.
+# https://leetcode.com/submissions/detail/591518349/
 
 from itertools import accumulate
 
