62b413f6dc21c0e90184ae648f872610b59d1c69
Alexander Bauer <94492708+alexanderbauer89@users.noreply.github.com>
2021-11-24 20:28:33 +0200

    Update 1929_Concatentation_of_Array.py

--- Problems/1929_Concatentation_of_Array.py
+++ Problems/1929_Concatentation_of_Array.py
@@ -1,3 +1,12 @@
+# alternative solution
+# Runtime: 119 ms, faster than 15.60% of Python3 online submissions for Concatenation of Array.
+# Memory Usage: 14.7 MB, less than 22.12% of Python3 online submissions for Concatenation of Array.
+# https://leetcode.com/submissions/detail/591528443/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums*2
+
 # Runtime: 98 ms, faster than 23.08% of Python3 online submissions for Concatenation of Array.
 # Memory Usage: 14.6 MB, less than 22.12% of Python3 online submissions for Concatenation of Array.
 # https://leetcode.com/submissions/detail/591518716/
