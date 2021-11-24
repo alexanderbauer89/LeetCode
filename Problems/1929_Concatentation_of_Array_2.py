8d50104ff64c87af60680062395c9f002a47388e
Alexander Bauer <94492708+alexanderbauer89@users.noreply.github.com>
2021-11-24 20:28:42 +0200

    Delete 1929_Concatentation_of_Array_2.py

--- Problems/1929_Concatentation_of_Array_2.py
+++
@@ -1,8 +0,0 @@
-# alternative solution to https://github.com/alexanderbauer89/LeetCode/blob/main/Problems/1929_Concatentation_of_Array.py
-# Runtime: 119 ms, faster than 15.60% of Python3 online submissions for Concatenation of Array.
-# Memory Usage: 14.7 MB, less than 22.12% of Python3 online submissions for Concatenation of Array.
-# https://leetcode.com/submissions/detail/591528443/
-
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        return nums*2
