2bced86c88d2c158d7c629dc5f9a85163bd0b609
Alexander Bauer <94492708+alexanderbauer89@users.noreply.github.com>
2021-11-24 23:19:34 +0200

    Create 0709_To_Lower_Case.py

---
+++ Problems/0709_To_Lower_Case.py
@@ -0,0 +1,7 @@
+# Runtime: 44 ms, faster than 15.92% of Python3 online submissions for To Lower Case.
+# Memory Usage: 14.3 MB, less than 37.04% of Python3 online submissions for To Lower Case.
+# https://leetcode.com/submissions/detail/592172278/
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
